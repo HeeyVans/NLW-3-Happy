340a5e6816080294e552f09f98a3f90b657d4d28
Vanessa Oliveira <vanessa.cs.oliveira@gmail.com>
2020-10-18 14:36:26 -0300

    Dia 4

--- public/scripts/page-orfanatos.js
+++ public/scripts/page-orfanatos.js
@@ -7,26 +7,43 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 })
 
-//create popup overlay
+function addMarker({id, name, lat, lng}){
+ 
+  //create popup overlay
 const popup = L.popup({
-    closeButton: false,
-    className: "map-popup",
-    minWidth: 240,
-    minHeight: 240,
-  }).setContent(
-    'Lar das meninas <a href="orfanato.html?id=1" class="choose-orfanatos"> <img src="./public/images/arrow-white.svg"> </a>'
-  );
+  closeButton: false,
+  className: "map-popup",
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  `${name} <a href="/orfanato?id=${id}"><img src="/images/arrow-white.svg"> </a>`
+);
 
 //create and add mark
 L
-.marker([-27.222633, -49.6455874],{icon})
+.marker([lat, lng],{icon})
 .addTo(map)
 .bindPopup(popup)
+}
+
+const orfanatosSpan = document.querySelectorAll('.orfanatos span')
+
+orfanatosSpan.forEach( span => {
+  const orfanato = {
+    id: span.dataset.id,
+    name: span.dataset.name, 
+    lat: span.dataset.lat,
+    lng: span.dataset.lng,
+
+  }
+
+  addMarker(orfanato)
+})
 
     
